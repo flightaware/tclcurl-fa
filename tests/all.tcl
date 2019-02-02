a2d9604cd31edafe0f3c775b1f9b6d98520e5fdc
Jeff Lawson <jeff.lawson@flightaware.com>
2019-02-02 08:04:38 +0000

    exit with non-zero value if any test fails

--- tests/all.tcl
+++ tests/all.tcl
@@ -13,8 +13,26 @@ package require tcltest
 
 ::tcltest::skipFiles [list]
 
+
+# Hook to determine if any of the tests failed. Then we can exit with
+# proper exit code: 0=all passed, 1=one or more failed
+proc tcltest::cleanupTestsHook {} {
+        variable numTests
+        set ::exitCode [expr {$numTests(Failed) > 0}]
+}
+
+
+
 # Allow command line arguments to be passed to the configure command
 # This supports only running a single test or a single test file
 ::tcltest::configure {*}$argv
 
 ::tcltest::runAllTests
+
+if {$exitCode == 1} {
+        puts "====== FAIL ====="
+        exit $exitCode
+} else {
+        puts "====== SUCCESS ====="
+}
+
